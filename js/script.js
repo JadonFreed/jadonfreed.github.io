4abf5a76bca03633e3bf025c5381e138de2350a7
JadonFreed <JWF46@pitt.edu>
2025-09-15 18:56:12 -0400

    pushhh

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Message sent!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
 });
